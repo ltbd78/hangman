3b29cf20f8df16e40f70787a0581deada6b96cd6
Linsu Han <ltbd78@gmail.com>
2019-11-04 09:03:03 -0500

    edited main

--- proj1.py
+++ proj1.py
@@ -8,25 +8,39 @@ def welcome():
 def pick():
     with open('words.txt') as f:
         lines = f.readlines()
-        word = random.choice(lines)
+        word = random.choice(lines).strip()
     return word
 
 
-def guess(char, word, placeholder):
+def guess(char, word, placeholder, attempts):
     p_list = list(placeholder)
     if char in word:
         for i in range(len(word)):
             if char == word[i]:
                 p_list[i] = char
-    return ''.join(p_list)
+    else:
+        attempts -= 1
+    return ''.join(p_list), attempts
 
 
 def play(word):
-    placeholder = '_' * len(word)
-    for i in range(10):
-        user_guess = input("Guess Letter: ")
-        placeholder = guess(user_guess, word, placeholder)
+    placeholder = '-' * len(word)
+    attempts = 10
+    while attempts > 0:
+        user_guess = input("Guess Letter: ").capitalize()
+        placeholder, attempts = guess(user_guess, word, placeholder, attempts)
         print(placeholder)
         if placeholder == word:
+            print('Win')
             return True
+        print("Attempts: ", attempts)
     return False
+
+
+def main():
+    welcome()
+    while True:
+        word = pick()
+        win = play(word)
+
+main()
